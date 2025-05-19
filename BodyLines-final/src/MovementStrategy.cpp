cdb06b0dd62ba3b0bf46fce44dbb9e3871a7a83c
ZhannaSargsyan <82668047+ZhannaSargsyan@users.noreply.github.com>
2025-05-19 12:46:38 +0400

    Delete BodyLines-final directory

--- BodyLines-final/src/MovementStrategy.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * @file MovementStrategy.cpp
- * @brief Implementation of the MovementStrategy abstract base class
- */
-#include "../include/MovementStrategy.h"
-
-// Since MovementStrategy is an abstract base class with implementation in the header,
-// no additional implementation is needed here.
